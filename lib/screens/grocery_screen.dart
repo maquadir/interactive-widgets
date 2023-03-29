f432b6b5644c06e369949602cb5c1248ff0cea45
adeemad <adeem@adabdigital.com>
2023-03-30 10:19:48 +1100

    Initial commit

---
+++ lib/screens/grocery_screen.dart
@@ -0,0 +1,14 @@
+
+
+import 'package:flutter/material.dart';
+
+class GroceryScreen extends StatelessWidget{
+  const GroceryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(color: Colors.green);
+  }
+
+}
